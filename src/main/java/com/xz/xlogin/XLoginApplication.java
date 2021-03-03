8c4195e7ea7736456fe6994a71e6fca3481013b0
xzlyf <czr2001@outlook.com>
2021-03-03 22:15:14 +0800

    first commit

---
+++ src/main/java/com/xz/xlogin/XLoginApplication.java
@@ -0,0 +1,13 @@
+package com.xz.xlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xz/xlogin/XloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xz.xlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
